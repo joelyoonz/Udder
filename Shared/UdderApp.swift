4a4e4fadc80d4c5b01e9902d388c02a184f5a454
Joel Yoon <joel.yoon@datacom.co.nz>
2021-12-10 13:40:03 +1300

    Initial Commit

---
+++ Shared/UdderApp.swift
@@ -0,0 +1,17 @@
+//
+//  UdderApp.swift
+//  Shared
+//
+//  Created by Joel Yoon on 10/12/21.
+//
+
+import SwiftUI
+
+@main
+struct UdderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
